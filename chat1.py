1b86ab9a49d37ec37044384d718c3c7b8bd7f8df
Tom Chan <tchan.cuhk@gmail.com>
2021-06-16 10:01:05 +0800

    sorting the chat update

--- chat1.py
+++ chat1.py
@@ -1,17 +1,26 @@
 # read the input file
 # write the output file
 chat = []
-name1 = 'Allen'
-name2 = 'Tom'
-with open('input.txt', 'r', encoding='utf-8') as f:
-	for line in f:
-		if name1 or name2 in line:
-			chat.append('\n' + line.strip() + ': ') 
-		else:
-			chat.append(line)
 
+def sort_chat(name1, name2): 
+	with open('input.txt', 'r', encoding='utf-8') as f:
+		for line in f:
+			if name1 in line:
+				speaker = name1
+			if name2 in line:
+				speaker = name2
+			if name1 in line:
+				continue
+			if name2 in line:
+				continue
+			chat.append(speaker + ': ' + line.strip() ) 	
+	return sort_chat
 
+def write_chat():
+	with open('output.txt', 'w', encoding='utf-8') as f:
+		for line in chat:
+			f.write(line + '\n')
+
+sort_chat('Allen', 'Tom')
+write_chat()
 print(chat)
-
-#name1: Allen
-#name2: Tom
