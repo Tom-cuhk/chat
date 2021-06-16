2c28723f23b1d76b8cddc2be3f66f784a6622d12
Tom Chan <tchan.cuhk@gmail.com>
2021-06-16 12:26:40 +0800

    update the chat conversion format

--- chat1.py
+++ chat1.py
@@ -1,6 +1,39 @@
 # read the input file
 # write the output file
-chat = []
+
+
+def read_file(filename):
+	lines = []
+	with open (filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+
+def convert(lines):
+	new = []
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		new.append(person + ': ' + line)
+	return new
+	
+def write_file(filename, lines):
+	with open(filename, 'w', encoding='utf-8') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+
+main()
+
 
 def sort_chat(name1, name2): 
 	with open('input.txt', 'r', encoding='utf-8') as f:
@@ -14,13 +47,10 @@ def sort_chat(name1, name2):
 			if name2 in line:
 				continue
 			chat.append(speaker + ': ' + line.strip() ) 	
-	return sort_chat
+	return chat
 
-def write_chat():
-	with open('output.txt', 'w', encoding='utf-8') as f:
-		for line in chat:
-			f.write(line + '\n')
 
-sort_chat('Allen', 'Tom')
-write_chat()
-print(chat)
+
+#sort_chat('Allen', 'Tom')
+# write_file()
+#print(chat)
